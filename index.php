9c09a883c66c69856be1ea2c1bbd7856e3cc89a2
Dimitri Kaiser <darkyne.gaming@outlook.de>
2020-09-25 03:14:58 +0200

    limited query to 3 items

--- index.php
+++ index.php
@@ -8,9 +8,26 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Darkyne | Home</title>
+        <link rel="stylesheet" href="./css/main.css">
     </head>
 
     <body>
-        
+        <p>Pulls the latest 3 entries in database in descending order</p>
+        <ul class="flex-list">
+            <?php 
+                $sql = "SELECT * FROM projects ORDER BY idProjects DESC LIMIT 3;";
+                $result = mysqli_query($connection, $sql);
+                $resultCheck = mysqli_num_rows($result);
+
+                if ($resultCheck > 0) {
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo "<li class='flex-item'>";
+                        echo "<h2>" . $row['ProjectName'] . "</h2>";
+                        echo "<p>" . $row['CreatedOn'] . "</p>";
+                        echo "</li>";
+                    }
+                }
+            ?>
+        </ul>
     </body>
 </html>
